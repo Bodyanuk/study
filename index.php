ad212ea233495805a387e595443455f0c08e1169
bodyanuk <bodyanuk@gmail.com>
2015-09-13 21:52:00 +0300

    getHeader goodbye

--- index.php
+++ index.php
@@ -4,7 +4,11 @@ $index = \includes\IndexController;
 
 $index->actionIndex();
 
+$index->getHeader();
+
 $index->setTemplate('/pages/foo'); // This is error place
 
 $index->goodBye();
+
+$index->endOfApplication();
 ?>
